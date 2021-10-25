52ac456227e6edd8f2e2ea32a1f5b515647f6323
thrivenichintha <58471724+thrivenichintha@users.noreply.github.com>
2021-10-25 23:15:46 +0530

    gsr (#14)
    * Create gsr
    
    * Rename gsr to gsr.ino
    
    * Rename gsr.ino to gsr/gsr.ino
    
    Co-authored-by: Adithya Sunil <adithyasunil26@gmail.com>

---
+++ gsr/gsr.ino
@@ -0,0 +1,19 @@
+const int GSR=A0;
+int sensorValue=0;
+int gsr_average=0;
+ 
+void setup(){
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  long sum=0;
+  for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
+      {
+      sensorValue=analogRead(GSR);
+      sum += sensorValue;
+      delay(5);
+      }
+   gsr_average = sum/10;
+   Serial.println(gsr_average);
+}
